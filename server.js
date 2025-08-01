fedf5aa11478aabef2ffd9e6317ca3c5be3f4fd2
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-08-01 15:16:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var port = process.env.PORT || 5934;
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
 
 function parseEnvList(env) {
   if (!env) {
@@ -20,8 +22,8 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: process.env.CORSANYWHERE_BLACKLIST,
-  originWhitelist: process.env.CORSANYWHERE_WHITELIST,
+  originBlacklist: originBlacklist,
+  originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
